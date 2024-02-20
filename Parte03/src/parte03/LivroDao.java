ee23d8581b0d5ea99357fa67878a4f8b638d90a2
Barbara Moroni <116181111+BaarbaraMoroni@users.noreply.github.com>
2024-02-19 22:08:47 -0300

    Create LivroDao.java

---
+++ Parte03/src/parte03/LivroDao.java
@@ -0,0 +1,13 @@
+package parte03;
+
+import java.util.List;
+
+// Interface para acesso aos dados relacionados à entidade Livro
+public interface LivroDAO {
+    // Métodos CRUD
+    public void adicionarLivro(Livro livro);
+    public void atualizarLivro(Livro livro);
+    public void removerLivro(int codigo);
+    public Livro buscarLivroPorCodigo(int codigo);
+    public List<Livro> buscarTodosLivros();
+}
